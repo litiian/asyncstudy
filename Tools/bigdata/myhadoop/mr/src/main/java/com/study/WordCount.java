4f13efd61206cab257163e37903a3a1232cdba61
tianli <tianli@zhangchuangsanyou.com>
2018-05-29 19:03:55 +0800

    增加过滤条件

--- Tools/bigdata/myhadoop/mr/src/main/java/com/study/WordCount.java
+++ Tools/bigdata/myhadoop/mr/src/main/java/com/study/WordCount.java
@@ -10,14 +10,25 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.StringUtils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.net.URI;
+import java.util.*;
 
 public class WordCount {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         Configuration conf=new Configuration();
+        GenericOptionsParser optionsParser=new GenericOptionsParser(conf,args);
+        String[] remainingArgs=optionsParser.getRemainingArgs();
+        if((remainingArgs.length!=2)&&(remainingArgs.length!=4)){
+            System.err.println("Usage:wordcount <int><out>[-skip skipParrernFile]");
+            System.exit(2);
+        }
 
         Job job=Job.getInstance(conf,"word count");
         job.setJarByClass(WordCount.class);
@@ -28,8 +39,19 @@ public class WordCount {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
+        List<String> otherArgs=new ArrayList<String>();
+        for (int i = 0; i <remainingArgs.length ; i++) {
+            if ("-skip".equals(remainingArgs[i])) {
+               job.addCacheFile(new Path(remainingArgs[++i]).toUri());
+               job.getConfiguration().setBoolean("wordcount.skip.parrerns",true);
+            }else{
+                otherArgs.add(remainingArgs[i]);
+            }
+        }
+
         FileInputFormat.addInputPath(job,new Path(args[0]));
         FileOutputFormat.setOutputPath(job,new Path(args[1]));
+
         System.exit(job.waitForCompletion(true)?0:1);
     }
 
@@ -40,11 +62,47 @@ public class WordCount {
         private Text word=new Text();
 
         private  final static IntWritable one=new IntWritable(1);
+        private boolean caseSensitive;
+        private Set<String> patternsToSkip =new HashSet<String>();
+
+        private  Configuration conf;
+        private BufferedReader fis;
+        @Override
+        public void  setup(Context context) throws IOException {
+           conf= context.getConfiguration();
+           caseSensitive=conf.getBoolean("wordcount.case.sensitive",true);
+            if(conf.getBoolean("wordcount.skip.patterns",false)){
+                URI[] patternsURIs=Job.getInstance(conf).getCacheFiles();
+                for(URI parrernsURI: patternsURIs){
+                    Path patternsPath=new Path(parrernsURI.getPath());
+                    String patternsFileName=patternsPath.getName().toString();
+                    parseSkipFile(patternsFileName);
+                }
+            }
+        }
+
+        private void parseSkipFile(String fileName) {
+            try {
+                fis=new BufferedReader(new FileReader(fileName));
+                String pattern=null;
+                while ((pattern=fis.readLine())!=null){
+                    patternsToSkip.add(pattern);
+                }
+            }catch (IOException ioe){
+                System.err.print("Caught exception while parsing the cached file"
+                        +StringUtils.stringifyException(ioe)
+                );
+            }
+        }
 
         @Override
         public void map(Object key,Text value,Context context) throws IOException, InterruptedException {
+            String line=caseSensitive?value.toString():value.toString().toLowerCase();
+            for(String pattern:patternsToSkip){
+                line=line.replaceAll(pattern,"");
+            }
             //StringTokenizer 默认使用 "空格"、"制表符（'\t'）"、"换行符（'\n'）"、"回车符（‘\r’）"
-            StringTokenizer itr=new StringTokenizer(value.toString());
+            StringTokenizer itr=new StringTokenizer(line);
             while (itr.hasMoreTokens()){
                 word.set(itr.nextToken());
                 context.write(word,one);
